b8f9483a1f7d1d643bf4197892547be9681e31e8
Bartek Maraszek <bm185154@ncr.com>
2015-05-07 17:48:54 +0100

    Add CRUD methods to MessageService

--- src/main/java/pl/bmaraszek/messenger/service/MessageService.java
+++ src/main/java/pl/bmaraszek/messenger/service/MessageService.java
@@ -1,26 +1,39 @@
 package pl.bmaraszek.messenger.service;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 
+import pl.bmaraszek.messenger.database.Database;
 import pl.bmaraszek.messenger.model.Message;
 
 public class MessageService {
 
-    public List<Message> getAllMessages(){
-        ArrayList<Message> result = new ArrayList<Message>();
-        Calendar c = Calendar.getInstance();
-        
-        c.set(2015, Calendar.MAY, 5);
-        result.add(new Message(1, new Message.MessageText("text 1"), c.getTime(), new Message.Author("Bartek")));
-        
-        c.set(Calendar.DAY_OF_MONTH, 6);
-        result.add(new Message(2, new Message.MessageText("text 2"), c.getTime(), new Message.Author("Bob")));
-        
-        c.set(Calendar.DAY_OF_MONTH, 7);
-        result.add(new Message(3, new Message.MessageText("text 3"), c.getTime(), new Message.Author("Alice")));
-        
-        return result;
+    private Map<Long, Message> messages = Database.getMessages();
+
+    public List<Message> getAllMessages() {
+        return new ArrayList<Message>(messages.values());
+    }
+
+    public Message getMessage(long id) {
+        return messages.get(id);
+    }
+
+    public Message addMessage(Message message) {
+        Message toAdd = new Message(message.getId() + 1, message.getMessage(), message.getCreated(), message.getAuthor());
+        messages.put(toAdd.getId(), message);
+        return toAdd;
+    }
+
+    public Message updateMessage(Message message) {
+        if (message.getId() <= 0) {
+            return null;
+        }
+        messages.put(message.getId(), message);
+        return message;
+    }
+    
+    public Message removeMessage(long id){
+        return messages.remove(id);
     }
 }
