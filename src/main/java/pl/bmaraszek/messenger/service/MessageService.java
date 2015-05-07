dbe13ca8ccf7ecbe86f420b4f747717170414378
Bartek Maraszek <bm185154@ncr.com>
2015-05-07 16:00:42 +0100

    Change message dates in getMessages()

--- src/main/java/pl/bmaraszek/messenger/service/MessageService.java
+++ src/main/java/pl/bmaraszek/messenger/service/MessageService.java
@@ -1,7 +1,7 @@
 package pl.bmaraszek.messenger.service;
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.List;
 
 import pl.bmaraszek.messenger.model.Message;
@@ -10,9 +10,17 @@ public class MessageService {
 
     public List<Message> getMessages(){
         ArrayList<Message> result = new ArrayList<Message>();
-        result.add(new Message(1, new Message.MessageText("text 1"), new Date(), new Message.Author("Bartek")));
-        result.add(new Message(2, new Message.MessageText("text 2"), new Date(), new Message.Author("Bob")));
-        result.add(new Message(3, new Message.MessageText("text 3"), new Date(), new Message.Author("Alice")));
+        Calendar c = Calendar.getInstance();
+        
+        c.set(2015, Calendar.MAY, 5);
+        result.add(new Message(1, new Message.MessageText("text 1"), c.getTime(), new Message.Author("Bartek")));
+        
+        c.set(Calendar.DAY_OF_MONTH, 6);
+        result.add(new Message(2, new Message.MessageText("text 2"), c.getTime(), new Message.Author("Bob")));
+        
+        c.set(Calendar.DAY_OF_MONTH, 7);
+        result.add(new Message(3, new Message.MessageText("text 3"), c.getTime(), new Message.Author("Alice")));
+        
         return result;
     }
 }
