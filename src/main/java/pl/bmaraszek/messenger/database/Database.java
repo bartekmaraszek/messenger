d6a9223b3c668ce7de2b4e8640b24105634285df
Bartek Maraszek <bm185154@ncr.com>
2015-05-07 17:37:31 +0100

    Add Database class

---
+++ src/main/java/pl/bmaraszek/messenger/database/Database.java
@@ -0,0 +1,20 @@
+package pl.bmaraszek.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pl.bmaraszek.messenger.model.Message;
+
+public class Database {
+
+    private static Map<Long, Message> messages = new HashMap<Long, Message>();
+    private static Map<Long, Profile> profiles = new HashMap<Long, Profile>();
+
+    public static Map<Long, Message> getMessages() {
+        return messages;
+    }
+
+    public static Map<Long, Profile> getProfiles() {
+
+    }
+}
